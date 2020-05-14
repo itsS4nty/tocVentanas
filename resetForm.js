11476150381f23350d85c48a54e0d5c954ccea7d
itsS4nty <50720295+itsS4nty@users.noreply.github.com>
2020-05-14 11:30:59 +0200

    Acabado

---
+++ resetForm.js
@@ -0,0 +1,6 @@
+function reset() {
+    document.getElementById('licencia').value = "";
+    document.getElementById('contraseña').value = "";
+    document.getElementById('impresora').selectedIndex = 0;
+    document.getElementById('datafono').selectedIndex = 0;
+}
